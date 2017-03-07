f9f61a901aca597dcf691fe8e3cf1805a7688b06
Shomi <somibenz@yahoo.com>
2017-03-07 14:44:06 -0500

    Add files via upload

---
+++ BMI/Week 4-5/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week_4_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnButton_Click(object sender, RoutedEventArgs e)
+        {
+            const int determiner = 703;
+            int weight = 0;
+            int height = 0;
+            int BMI = weight * determiner;
+            int txtAmount = int.Parse(txtBox.Text);
+            int txtAmount1 = int.Parse(txtBox1.Text);
+            if (BMI >= 18.5 && BMI < 25)
+            {
+                lblOutput.Content = ("You are in top shape");
+            }
+             else if (BMI< 18.5)
+            {
+                lblOutput.Content = ("You are underweight");
+            }
+             else if (BMI>18.5)
+                lblOutput.Content=("You are grossly overweight");
+                    
+                
+
+            
+           
+        }
+    }
+}
